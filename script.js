3a39cdf19358d95a0297cb6bd92b85642330e4a1
Martinelli303 <martinelli303@hotmail.com>
2023-05-10 10:59:35 -0300

    scrollAnimation

--- script.js
+++ script.js
@@ -62,3 +62,27 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+function initiAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.lenght) {
+    const windowMetade = window.innerHeight * 0.55;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+initiAnimacaoScroll();
